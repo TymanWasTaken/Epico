28a84ea54e39df51993c4d54e424c7f9f5cae09a
TymanWasTaken <32660892+TymanWasTaken@users.noreply.github.com>
2022-03-04 19:10:40 -0700

    kit moment
    not finishing for now because idk if it works with yarn pnp

---
+++ playwright.config.js
@@ -0,0 +1,9 @@
+/** @type {import('@playwright/test').PlaywrightTestConfig} */
+const config = {
+	webServer: {
+		command: 'yarn run build && yarn run preview',
+		port: 3000
+	}
+};
+
+export default config;
